2d9b1f687270369bbac11e2d8969a974d649ddb9
Vincentius Franklin <vincentiusf17@gmail.com>
2026-05-15 23:15:12 +0700

    feat: add vercel wrapper

--- api/src/index.ts
+++ api/src/index.ts
@@ -24,6 +24,10 @@ app.get("/", (_, res) => {
 
 app.use("/api", routes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+export default app;
