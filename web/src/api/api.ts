13ebce27b74ea264ba0f4ae39138ca54b9c781c8
Vincentius Franklin <vincentiusf17@gmail.com>
2026-05-17 13:29:42 +0700

    fix: changes

--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -1,6 +1,5 @@
 const BASE_URL =
   process.env.NEXT_PUBLIC_API_URL || "https://bobtester-u9xe.vercel.app/api";
-// const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api";
 
 async function request(endpoint: string, options: RequestInit = {}) {
   const token =
