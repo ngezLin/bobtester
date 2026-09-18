53b60d1441a8b011b14c53abf11f7930bb6e1f60
ngezLin <89445359+ngezLin@users.noreply.github.com>
2026-09-19 03:06:59 +0700

    refactor: remove common.js facade and use SubCommon directly

--- projects/pos_check_login/main.js
+++ projects/pos_check_login/main.js
@@ -1,8 +1,8 @@
 const { bob } = require("bobtester");
-const common = require("../utils/common");
+const sub = require("../utils/SubCommon");
 
 bob.run(async () => {
-  await common.login.standard_user();
+  await sub.login.standard_user();
   await bob.screenshot("logged_in_dashboard");
-  await common.logout();
+  await sub.logout();
 });
