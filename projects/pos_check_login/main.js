5eec353dcfa4b557d8aae54ad53623b1e9a5a2b4
ngezLin <89445359+ngezLin@users.noreply.github.com>
2026-09-19 03:19:32 +0700

    test: add screenshot and logout between login attempts

--- projects/pos_check_login/main.js
+++ projects/pos_check_login/main.js
@@ -2,7 +2,13 @@ const { bob } = require("bobtester");
 const sub = require("../utils/SubCommon");
 
 bob.run(async () => {
-  await sub.login.standard_user();
-  await bob.screenshot("logged_in_dashboard");
+  await sub.login("standard_user");
+  
+  // Capture screenshot after logging in
+  await bob.screenshot("logged_in");
+  
+  // Note: You must log out before you can log in as a different user!
   await sub.logout();
+
+  await sub.login("test");
 });
